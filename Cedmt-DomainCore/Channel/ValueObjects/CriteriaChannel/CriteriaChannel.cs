0c5feaf455396dcad661440587c4e4a6fd75d685
MatiasMeierCedeira <matias.meier@cedeira.com.ar>
2024-12-19 13:33:30 -0300

    feat: se sumo la entidad principal channelEntity y se comenzó agregar una proto estructura de valueObjects

---
+++ Cedmt-DomainCore/Channel/ValueObjects/CriteriaChannel/CriteriaChannel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cedmt_DomainCore.Channel.ValueObjects.CriteriaChannel
+{
+    public class CriteriaChannel
+    {
+        public CriteriaChannel() { }
+    }
+}
---
+++ Cedmt-DomainCore/Channel/ValueObjects/LastSync.cs
@@ -0,0 +1,16 @@
+﻿using Cedeira.Essentials.NET.Diagnostics.Invariants;
+
+namespace Cedmt_DomainCore.ValueObjects
+{
+    public class LastSync : ValueObjects<DateTime>
+    {
+        public LastSync(DateTime value) : base(value) { }
+        
+        public static LastSync Create(DateTime value) 
+        {
+            Invariants.For(value).IsNotNull();  
+
+            return new LastSync(value);     
+        }    
+    }
+}
